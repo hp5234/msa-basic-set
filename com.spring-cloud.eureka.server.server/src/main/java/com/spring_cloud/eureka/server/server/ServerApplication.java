f4025aacce804bc27a269156e4a7071d329a99f8
jeonjinwoo <hp5234@naver.com>
2025-02-10 10:31:28 +0900

    서버측 초기 설정

--- com.spring-cloud.eureka.server.server/src/main/java/com/spring_cloud/eureka/server/server/ServerApplication.java
+++ com.spring-cloud.eureka.server.server/src/main/java/com/spring_cloud/eureka/server/server/ServerApplication.java
@@ -2,7 +2,9 @@ package com.spring_cloud.eureka.server.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // 유레카 서버 동작 활성화
 @SpringBootApplication
 public class ServerApplication {
 
